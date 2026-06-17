3821cd2a5f73a7e70c537aae15562d614cc3239a
Aston Lawrence <aston.lawrence.nz@gmail.com>
2026-06-17 20:47:37 +1200

    Initialise git and go

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use crate::args::InitArgs;
 use clap::Parser;
+use std::process::Command;
 
 mod args;
 
 fn main() {
     // Get the user input
     let args = InitArgs::parse();
+
+    // Initialise git
+    Command::new("git").arg("init").status().expect("Failed to initialise git");
+
+    // Initialise a golang module
+    Command::new("go").args(["mod", "init", args.module_name.as_str()]).status().expect("Failed to initialise git");
 }
