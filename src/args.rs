69aff7232845c1c327cd5814510e4fe8ca557665
Aston Lawrence <aston.lawrence.nz@gmail.com>
2026-06-18 17:04:18 +1200

    Add `new` command and move existing logic to `init` command

--- src/args.rs
+++ src/args.rs
@@ -1,8 +1,30 @@
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 
 #[derive(Debug, Parser)]
 #[command(version, about)]
+pub struct GoGitArgs {
+    #[command(subcommand)]
+    pub command: GoGitCommand,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum GoGitCommand {
+    /// Initialises golang and git in the current directory.
+    Init(InitArgs),
+    /// Creates a new subdirectory and initialises it with golang and git.
+    New(NewArgs),
+}
+
+#[derive(Args, Debug)]
 pub struct InitArgs {
-    /// The module name (passed to `go mod init`)
+    /// The module name (passed to go mod init).
+    pub module_name: String,
+}
+
+#[derive(Args, Debug)]
+pub struct NewArgs {
+    /// The name of the subdirectory to create.
+    pub dir: String,
+    /// The module name (passed to go mod init).
     pub module_name: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,40 @@
-use crate::args::InitArgs;
+use crate::args::{GoGitArgs, GoGitCommand};
 use clap::Parser;
+use std::env::set_current_dir;
+use std::fs::create_dir;
 use std::process::Command;
 
 mod args;
 
 fn main() {
     // Get the user input
-    let args = InitArgs::parse();
+    let args = GoGitArgs::parse();
 
+    match args.command {
+        GoGitCommand::Init(args) => init(args.module_name),
+        GoGitCommand::New(args) => {
+            // Make the target dir
+            create_dir(args.dir.clone()).expect("Failed to create target directory.");
+
+            // Set the program's cwd to the new dir
+            set_current_dir(args.dir).expect("Failed to change directory.");
+
+            init(args.module_name);
+        }
+    }
+}
+
+/// Initialises golang and git for the cwd.
+fn init(module_name: String) {
     // Initialise git
-    Command::new("git").arg("init").status().expect("Failed to initialise git");
+    Command::new("git")
+        .arg("init")
+        .status()
+        .expect("Failed to initialise git.");
 
     // Initialise a golang module
-    Command::new("go").args(["mod", "init", args.module_name.as_str()]).status().expect("Failed to initialise git");
+    Command::new("go")
+        .args(["mod", "init", module_name.as_str()])
+        .status()
+        .expect("Failed to initialise git.");
 }
